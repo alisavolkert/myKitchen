10c1b3fe1e2659172e4b74b98ce3447687084d46
alisavolkert <alisa.volkert@uni-tuebingen.de>
2019-07-29 17:46:59 +0200

    Hinzufügen von Impressum und Datenschutzerklärung-beide unfertig

---
+++ impressum.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="de"><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+  <meta charset="utf-8">
+
+  <link rel="stylesheet" href="impressum-Dateien/bootstrap.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+  <script src="impressum-Dateien/jquery-3.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="impressum-Dateien/popper.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="impressum-Dateien/bootstrap.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+</head>
+
+
+<body>
+
+
+
+  
+
+
+  <div class="container">
+    <!-- <button class="btn btn-primary mt-5" onclick="window.location.href='https://micro-europa.de'">Zurück zur Webseite</button> -->
+
+    <h1 class="mt-5">Impressum für "ProKRep-Küchensimulation"</h1>
+
+
+        <h3 id="kontakt" class="mt-5">Allgemeine Informationen gem. § 5 TMG, § 55 RStVG</h3>
+        <p>
+          Universität Tübingen<br>
+          Fachbereich Informatik<br>
+          Arbeitsgruppe HCI & AI<br>
+          Alisa Volkert<br>
+          Sand 14<br>
+          72076 Tübingen<br>
+        </p><p>
+          <b>Telefon:</b> +49 7071 29-70419<br>
+          <b>Email:</b> alisa.volkert (@) uni-tuebingen.de</p>
+
+<!-- TODO: checken, ob das hier inhaltlich so passt -->
+  <h3 class="mt-5">Urheberrecht</h3>
+ <p>Alle auf dieser Website veröffentlichten Inhalte (Layout, Texte, 
+Bilder, Grafiken, usw.) unterliegen dem
+Urheberrecht. Jede vom Urheberrechtsgesetz nicht zugelassene Verwertung 
+bedarf vorheriger ausdrücklicher Zustimmung der Universität Tübingen. Dies gilt
+insbesondere für Vervielfältigung, Bearbeitung, Übersetzung, 
+Einspeicherung, Verarbeitung bzw. Wiedergabe von Inhalten in Datenbanken
+ oder anderen elektronischen Medien und Systemen. Fotokopien und 
+Downloads von Web-Seiten für den privaten, wissenschaftlichen und nicht 
+kommerziellen Gebrauch dürfen hergestellt werden. Bilder von externen 
+Quellen sind mit entsprechendem Hinweis auf den Urheber versehen.</p>
+
+<h3 class="mt-5">Haftungsausschluss</h3>
+<p>Die Informationen auf dieser Website wurden nach bestem Wissen und 
+Gewissen sorgfältig zusammengestellt und geprüft. Es wird jedoch keine 
+Gewähr – weder ausdrücklich noch stillschweigend – für die 
+Vollständigkeit, Richtigkeit oder Aktualität sowie die jederzeitige 
+Verfügbarkeit der bereit gestellten Informationen übernommen. Eine 
+Haftung für Schäden, die aus der Nutzung oder Nichtnutzung der auf 
+dieser Website angebotenen Informationen entstehen ist – soweit 
+gesetzlich zulässig – ausgeschlossen.</p>
+
+          
+
+
+
+
+                                            <button class="btn btn-primary mt-5 mb-5" onclick="window.location.href='https://micro-europa.de'">Zurück zur Webseite</button>
+
+
+
+
+
+                                          </div>
+
+
+                                        
+</body></html>
